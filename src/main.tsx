936d81629446822d125fe10d2e559bb0458b01ff
Eckintosh <4ai360@gmail.com>
2026-09-15 22:30:47 +0000

    Add app entry point, global styles and utilities

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import AdminApp from "./admin/AdminApp";
+import { SiteContentProvider } from "./lib/siteContent";
+
+const isAdminRoute = window.location.hash.startsWith("#/admin");
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <SiteContentProvider>{isAdminRoute ? <AdminApp /> : <App />}</SiteContentProvider>
+  </StrictMode>
+);
